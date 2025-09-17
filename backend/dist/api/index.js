16e3523442c9c23c50c789e83036e704bcd1f5f5
Abhisheksavy <abhi804d@gmail.com>
2025-09-17 13:01:26 +0530

    backend structure changed

---
+++ backend/dist/api/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from 'dotenv';
+import router from "../routes/api.routes.js";
+import connectDB from "../db/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+dotenv.config();
+const PORT = process.env.PORT;
+const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api", router);
+app.get("/api/test", (req, res) => {
+    res.json({ message: "Backend is working!" });
+});
+async function start() {
+    await connectDB();
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+start();
+export default app;
+//# sourceMappingURL=index.js.map
