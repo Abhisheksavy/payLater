5119611f921c48b772858beb937191c10e4fec0d
abhi <abhi804d@gmail.com>
2025-09-18 18:32:19 +0530

    push

--- backend/src/middlewares/AuthCheck.ts
+++ backend/src/middlewares/AuthCheck.ts
@@ -5,14 +5,23 @@ export interface AuthRequest extends Request {
   userId?: string;
 }
 
-export const authCheck = (req: AuthRequest, res: Response, next: NextFunction) => {
+export const authCheck = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const token = req.cookies?.token;
-    console.log("req.body?.userId",req.body?.userId)
-    const decoded = verifyAccessToken<{ id: string }>(token);
-    req.userId = decoded.id || req.body?.userId;
-
-    next();
+    if (token) {
+      const decoded = verifyAccessToken<{ id: string }>(token);
+      req.userId = decoded.id;
+      return next();
+    } else if (req.body?.userId) {
+      req.userId = req.body.userId;
+      return next();
+    } else {
+      return res.status(404).json({ message: "No token or userId provided" });
+    }
   } catch (err) {
     return res.status(400).json({ message: "Token is not valid" });
   }
